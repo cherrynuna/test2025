12a38db65bcb4a2d3c08415261e9eb2e07c35135
cherrynuna <inyoungstar7@gmail.com>
2025-05-18 16:00:23 +0900

    no env

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const http = require('http');
 
+const PORT = process.env.FASTIFY_PORT || 3000;
+
 const server = http.createServer((req, res) => {
   res.end('Hello, Docker!');
 });
 
-server.listen(3000, () => {
-  console.log('Server is running on port 3000');
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
