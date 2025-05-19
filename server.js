d933c8848c59102549bdf1b96426895eaafdeb16
cherrynuna <inyoungstar7@gmail.com>
2025-05-19 13:54:23 +0900

    test4

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const http = require('http');
 
+console.log(process.env);
+
 const PORT = process.env.FASTIFY_PORT;
+const TEST_ENV = process.env.TEST_ENV;
 
 if (!PORT) {
   throw new Error('환경변수 FASTIFY_PORT가 설정되어 있지 않습니다.');
+}
+if (!TEST_ENV) {
+  throw new Error('환경변수 TEST_ENV가 설정되어 있지 않습니다.');
 }
 
 const server = http.createServer((req, res) => {
